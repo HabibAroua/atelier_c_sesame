ee4e973b480b4184bfe89533565793188fd09682
HabibAroua <habibha.aroua82@gmail.com>
2017-09-29 20:49:47 +0100

    first commit

--- first/main.c
+++ first/main.c
@@ -3,8 +3,16 @@
 
 int main()
 {
-    printf("This is my first program in sesame \n");
+
+    printf("donner un nombre \n");
+    char x;
+    scanf("%c",&x);
+    x++;
+    //int res=x+1;
+    printf("%c \n",x);
+   // printf("%d \n",x*x);
     getchar();
+    sleep(1000);
     return 0;
 }
 //Habib Aroua
