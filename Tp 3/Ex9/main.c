be754ae103831633cca6dcdfa79d17d4425961a9
HabibAroua <habibha.aroua82@gmail.com>
2017-10-19 21:26:14 +0100

    other tache

---
+++ Tp 3/Ex9/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int u;
+    printf("Donner un terme\n");
+    scanf("%d",&u);
+    int i=1;
+    while(i!=10)
+    {
+
+       if(u%2==0)
+       {
+           u=u-(1/2);
+           printf("le terme %d est %d \n",i,u);
+       }
+       else
+       {
+           u=3*u;
+           printf("le terme %d est %d \n",i,u);
+       }
+       i++;
+    }
+    return 0;
+}
