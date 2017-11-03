c070f06a439dc1671edbd28f0d077de62a74fc31
HabibAroua <habibha.aroua82@gmail.com>
2017-11-03 16:15:24 +0100

    Modifier le nom des variables

--- Tp 4/Ex5/main.c
+++ Tp 4/Ex5/main.c
@@ -3,7 +3,7 @@ int main()
 {
     int i,j;
     float mat[3][3];
-    float moyLigne,moyColone,moyGlobale;
+    float sommeLigne,sommeColone,sommeGlobale;
     for(i=0;i<3;i++)
     {
        for(j=0;j<3;j++)
@@ -12,29 +12,30 @@ int main()
             scanf("%f",&mat[i][j]);
        }
     }
-    moyLigne=0;
+    sommeLigne=0;
 
     for(i=0;i<3;i++)
     {
        for(j=0;j<3;j++)
        {
-            moyLigne+=mat[i][j];
+            sommeLigne+=mat[i][j];
        }
-       printf("La moyenne de la ligne %d est %f \n ",i+1,moyLigne/3);
+       printf("La moyenne de la ligne %d est %f \n ",i+1,sommeLigne/3);
        printf("\n");
     }
-    moyColone=0;
-    moyGlobale=0;
+    sommeColone=0;
+    sommeGlobale=0;
     for(j=0;j<3;j++)
     {
        for(i=0;i<3;i++)
        {
-            moyColone+=mat[i][j];
-            moyGlobale+=mat[i][j];
+            sommeColone+=mat[i][j];
+            sommeGlobale+=mat[i][j];
        }
-       printf("La moyenne de la colone %d est %f \n ",j+1,moyColone/3);
+       printf("La moyenne de la colone %d est %f \n ",j+1,sommeColone/3);
        printf("\n");
     }
-    printf("Le moyenne globale est %f \n",moyGlobale/9);
+    printf("Le moyenne globale est %f \n",sommeGlobale/9);
     return 0;
 }
+
