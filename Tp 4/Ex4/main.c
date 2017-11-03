459b3481f7c7c0671a8a31c9423d654f0736af18
HabibAroua <habibha.aroua82@gmail.com>
2017-11-03 14:34:59 +0100

    modifier exercice trangle de pascal

--- Tp 4/Ex4/main.c
+++ Tp 4/Ex4/main.c
@@ -7,7 +7,7 @@ int main()
     {
        printf("Donner un nombre n \n");
        scanf("%d",&n);
-    }while((n<0)||(n>100));
+    }while((n<2)||(n>100));
     for(i=0 ;i<n;i++)
     {
        for(j=0;j<i;j++)
