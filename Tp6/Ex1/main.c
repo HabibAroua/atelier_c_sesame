729d44f2486c2822c15d9cdbd8ad3571ff903233
HabibAroua <habibha.aroua82@gmail.com>
2017-11-20 14:47:19 +0100

    Tp N°6 terminer fonction PlusSouvent()

---
+++ Tp6/Ex1/main.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+
+int tab[10];
+void SaisirTab(int tab[10],int n);
+int PlusSouvent(int tab[10],int n);
+
+int main()
+{
+    int tab[100];
+    int n;
+    printf("Donner n \n");
+    scanf("%d",&n);
+    SaisirTab(tab,n);
+    printf("La valeur qui apparait le plus souvent dans le tableau est %d \n",PlusSouvent(tab,n));
+    return 0;
+}
+
+void SaisirTab(int tab[10], int n)
+{
+   int i;
+   for(i=0;i<n;i++)
+   {
+
+       printf("tapez la case %d \n",i+1);
+       scanf("%d",&tab[i]);
+   }
+}
+
+int PlusSouvent(int tab[10],int n)
+{
+    int i,j,occ,nocc,ps,eps;
+
+    ps=0;
+    eps=0;
+    occ=0;
+    nocc=0;
+
+    for(i=0;i<n;i++)
+    {
+        occ=tab[i];
+        for(j=0;j<n;j++)
+        {
+            if(tab[j]==occ)
+            {
+               nocc++;
+            }
+        }
+        if(nocc>ps)
+        {
+           ps=nocc;
+           eps=occ;
+        }
+        nocc=0;
+    }
+    return eps;
+}
