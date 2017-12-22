b962bdf90906d6e5f87ffcdb4ebf7831038c97c3
HabibAroua <habibha.aroua82@gmail.com>
2017-12-22 22:18:13 +0100

    remplir la taille et tableau avec pointeur

--- tri/tri/main.c
+++ tri/tri/main.c
@@ -1,10 +1,35 @@
 #include <stdio.h>
-void saisir(int n);
-void remplir(int n,int t[20]);
-void affiche(int n,int t[20]);
+void saisir(int *n);
+void remplir(int n,int *t);
 
 int main()
 {
-    printf("Hello world!\n");
+    int n=10;
+    int t[20];
+    saisir(&n);
+    remplir(n,&t);
     return 0;
+}
+
+void saisir(int *n)
+{
+    int x;
+    do
+    {
+        printf("Donner la taille de tableau \n");
+        scanf("%d",&x);
+    }while((x>20)||(x<1));
+    *n=x;
+}
+
+void remplir(int n,int *t)
+{
+     int x=0;
+     for(int i=0;i<n;i++)
+     {
+          printf("Tapez la case %d \n",i);
+          scanf ("%d", &x);
+          *t = x;
+     }
+     printf("la valeur de n est %d \n",n);
 }
