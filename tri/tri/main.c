ddb2e998ae0d0dfeb0bfdb1be8194f0257369102
HabibAroua <habibha.aroua82@gmail.com>
2017-12-24 00:56:11 +0100

    débuté  project

--- tri/tri/main.c
+++ tri/tri/main.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 void saisir(int *n);
 void remplir(int n,int *t);
-
+void afficher(int n,int t[20]);
 int main()
 {
     int n=10;
-    int t[20];
+    int t[20]={0};
     saisir(&n);
     remplir(n,&t);
+    afficher(n,&t);
     return 0;
 }
 
@@ -32,4 +33,15 @@ void remplir(int n,int *t)
           *t = x;
      }
      printf("la valeur de n est %d \n",n);
+}
+
+void afficher(int n,int t[20])
+{
+     int x;
+     printf("L'affichage de tableau \n");
+     for(int i=0;i<n;i++)
+     {
+          x=&t[i];
+          printf("%d | ",x);
+     }
 }
