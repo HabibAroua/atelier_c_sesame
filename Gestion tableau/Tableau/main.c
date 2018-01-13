17fc02de4b03ad7480a75a348bfa52da67b02132
HabibAroua <habibha.aroua82@gmail.com>
2018-01-13 20:03:36 +0100

    Remplir tableau

--- Gestion tableau/Tableau/main.c
+++ Gestion tableau/Tableau/main.c
@@ -1,8 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void saisie(int *n);
+void remplir(int n, int *tab);
+void afficher(int n,int tab[50]);
+
 int main()
 {
-    printf("Hello world!\n");
+    int n;
+    int tab[50];
+    saisie(&n);
+    remplir(n,tab);
+    afficher(n,tab);
     return 0;
+}
+
+void saisie(int *n)
+{
+    int x;
+    do
+    {
+        printf("Donner la taille du tableau \n");
+        scanf("%d",&x);
+    }while((x>50)||(x<1));
+    *n=x;
+}
+
+void remplir(int n, int *tab)
+{
+    int i;
+    for (i=0;i<=n;i++)
+    {
+         printf("Tapez la case %d \n",i);
+         scanf("%d",&tab[i]);
+    }
+}
+
+void afficher(int n,int tab[50])
+{
+     int i;
+     for(i=0;i<=n;i++)
+     {
+         printf("%d |",tab[i]);
+     }
 }
