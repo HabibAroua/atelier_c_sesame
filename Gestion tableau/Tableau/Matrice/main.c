969933126977ffbcdff7f36f30af1f788255939d
HabibAroua <habibha.aroua82@gmail.com>
2018-01-23 15:50:18 +0100

    gerer une  matrice

---
+++ Gestion tableau/Tableau/Matrice/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void saisie(int *n);
+void remplir(int n , int *M);
+void afficher(int n , int M[20][20]);
+
+int main()
+{
+    int n;
+    printf("Hello world!\n");
+    return 0;
+}
+
+void saisie(int *n)
+{
+}
+
+void remplir(int n,int *M)
+{
+}
+
+void afficher(int n , int M[20][20])
+{
+}
