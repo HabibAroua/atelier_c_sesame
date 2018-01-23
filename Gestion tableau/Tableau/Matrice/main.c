3563d9577a6755f1aafca38e0b75fe6c5a5ed758
HabibAroua <habibha.aroua82@gmail.com>
2018-01-23 16:17:03 +0100

    Matrice file

--- Gestion tableau/Tableau/Matrice/main.c
+++ Gestion tableau/Tableau/Matrice/main.c
@@ -2,24 +2,54 @@
 #include <stdlib.h>
 
 void saisie(int *n);
-void remplir(int n , int *M);
+void remplir(int n , int M[20][20]);
 void afficher(int n , int M[20][20]);
 
 int main()
 {
     int n;
-    printf("Hello world!\n");
+    int M[20][20];
+    saisie(&n);
+    remplir(n,M);
+    afficher(n,M);
     return 0;
 }
 
 void saisie(int *n)
 {
+    int x;
+    do
+    {
+        printf("Donner la taille de la matrice carree \n");
+        scanf("%d",&x);
+    }while((x>20)||(x<1));
+    *n=x;
 }
 
-void remplir(int n,int *M)
+void remplir(int n , int M[20][20])
 {
+     int i,j;
+     int x;
+     for(i=0 ; i<n ; i++)
+     {
+          for(j=0 ; j<n ; j++)
+          {
+              printf("Tapez la case %d de la ligne %d ",j,i);
+              scanf("%d",&x);
+              M[i][j]=x;
+          }
+     }
 }
 
 void afficher(int n , int M[20][20])
 {
+    int i,j;
+    for(i=0 ; i<n ; i++)
+    {
+        for(j=0 ; j<n ; j++)
+        {
+              printf("%d ",M[i][j]);
+        }
+        printf("\n");
+    }
 }
