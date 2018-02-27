6210441812272cb45fa38a51942f2f9f4b1532fc
HabibAroua <habibha.aroua82@gmail.com>
2018-02-27 19:20:01 +0100

    Create liste add one element

--- listeChainne/Exemple/main.c
+++ listeChainne/Exemple/main.c
@@ -6,11 +6,14 @@ struct Maillon
    int valeur;
    struct Maillon *suivant;
 };
+
 typedef struct Maillon ListeEntier;
-ListeEntier *L ;
 void ListeVide(ListeEntier *L);
+void creeListe(ListeEntier *L);
 int main()
 {
+   ListeEntier *L ;
+   ListeVide(L);
 
 
     return 0;
@@ -19,4 +22,14 @@ int main()
 void ListeVide(ListeEntier *L)
 {
      L=NULL;
+}
+
+void creeListe(ListeEntier *L)
+{
+    ListeEntier *p;
+    p=(ListeEntier*)malloc(sizeof(ListeEntier));
+    printf ("Entrer un entier \n");
+    scanf ("%d", &(p->valeur));
+    p->suivant=NULL;
+    L=p;
 }
