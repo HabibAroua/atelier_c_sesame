62ea39ce97906dc914c9dacf6cf8a426347a5c77
HabibAroua <habibha.aroua82@gmail.com>
2018-02-27 19:12:49 +0100

    Declare out of main function

--- listeChainne/Exemple/main.c
+++ listeChainne/Exemple/main.c
@@ -1,13 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+struct Maillon
+{
+   int valeur;
+   struct Maillon *suivant;
+};
+typedef struct Maillon ListeEntier;
+ListeEntier *L ;
+void ListeVide(ListeEntier *L);
 int main()
 {
-    struct Maillon
-    {
-         int valeur;
-         struct Maillon *suivant;
-    };
+
+
     return 0;
 }
 
+void ListeVide(ListeEntier *L)
+{
+}
