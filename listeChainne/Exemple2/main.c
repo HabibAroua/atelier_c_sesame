0da1c79530841fb290e4a2aa8aaf4c4a66376b64
HabibAroua <habibha.aroua82@gmail.com>
2018-03-15 21:47:12 +0100

    function add element in the last of the list

--- listeChainne/Exemple2/main.c
+++ listeChainne/Exemple2/main.c
@@ -10,6 +10,7 @@ struct element
 
 typedef element* llist;
 llist ajouterEnTete(llist liste, int valeur);
+llist ajouterEnFin(llist liste, int valeur);
 
 int main()
 {
@@ -27,4 +28,26 @@ llist ajouterEnTete(llist liste, int valeur)
     nouvelElement->val = valeur;
     nouvelElement->nxt = liste;
     return nouvelElement;
+}
+
+llist ajouterEnFin(llist liste, int valeur)
+{
+    element* nouvelElement = malloc(sizeof(element));
+    nouvelElement->val = valeur;
+    nouvelElement->nxt = NULL;
+
+    if(liste == NULL)
+    {
+        return nouvelElement;
+    }
+    else
+    {
+        element* temp=liste;
+        while(temp->nxt != NULL)
+        {
+            temp = temp->nxt;
+        }
+        temp->nxt = nouvelElement;
+        return liste;
+    }
 }
