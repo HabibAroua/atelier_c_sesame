089fa79ded797db5341ec9c389879e24c0ac9ed2
HabibAroua <habibha.aroua82@gmail.com>
2018-03-15 21:40:34 +0100

    Decalare chainn list

--- listeChainne/Exemple2/main.c
+++ listeChainne/Exemple2/main.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct element element;
+struct element
+{
+    int val;
+    struct element *nxt;
+};
+
+typedef element* llist;
+
 int main()
 {
     printf("Hello world!\n");
