e773ffe3ce0e16d6c20dc39a80fa676518125cf9
HabibAroua <habibha.aroua82@gmail.com>
2018-03-30 13:16:19 +0100

    defiler

--- Piles & File/File/main.c
+++ Piles & File/File/main.c
@@ -1,21 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct Element Element;
+struct Element
+{
+    int nombre;
+    Element *suivant;
+};
+
+typedef struct File File;
+struct File
+{
+    Element *premier;
+};
+void enfiler(File *file, int nvNombre);
+int defiler(File *file);
+
 int main()
 {
-    typedef struct Element Element;
-    struct Element
-    {
-        int nombre;
-        Element *suivant;
-    };
-
-    typedef struct File File;
-    struct File
-    {
-          Element *premier;
-    };
-
     int choix;
     do
     {
@@ -45,4 +47,53 @@ int main()
          }while((choix<1)||(choix>4));
     }while(choix!=4);
     return 0;
+}
+
+void enfiler(File *file, int nvNombre)
+{
+    Element *nouveau = malloc(sizeof(*nouveau));
+    if (file == NULL || nouveau == NULL)
+    {
+        exit(EXIT_FAILURE);
+    }
+
+    nouveau->nombre = nvNombre;
+    nouveau->suivant = NULL;
+
+    if (file->premier != NULL) /* La file n'est pas vide */
+    {
+        /* On se positionne à la fin de la file */
+        Element *elementActuel = file->premier;
+        while (elementActuel->suivant != NULL)
+        {
+            elementActuel = elementActuel->suivant;
+        }
+        elementActuel->suivant = nouveau;
+    }
+    else /* La file est vide, notre élément est le premier */
+    {
+        file->premier = nouveau;
+    }
+}
+
+int defiler(File *file)
+{
+    if (file == NULL)
+    {
+        exit(EXIT_FAILURE);
+    }
+
+    int nombreDefile = 0;
+
+    /* On vérifie s'il y a quelque chose à défiler */
+    if (file->premier != NULL)
+    {
+        Element *elementDefile = file->premier;
+
+        nombreDefile = elementDefile->nombre;
+        file->premier = elementDefile->suivant;
+        free(elementDefile);
+    }
+
+    return nombreDefile;
 }
