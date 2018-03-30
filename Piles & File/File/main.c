608c1b3b4e564df426702146500634671b855736
HabibAroua <habibha.aroua82@gmail.com>
2018-03-30 12:55:58 +0100

    test Menu

--- Piles & File/File/main.c
+++ Piles & File/File/main.c
@@ -16,6 +16,19 @@ int main()
               printf("________________________\n");
               printf("Donner votre choix \n");
               scanf("%d",&choix);
+              switch(choix)
+              {
+                  case 1 : printf("Emfiler \n");
+                  break;
+                  case 2 : printf("Defiler \n");
+                  break;
+                  case 3 : printf("Afficher \n");
+                  break;
+                  case 4 : printf("Vous quittez l'application \n");
+                  break;
+                  default : printf("Choix invalide \n");
+                  break;
+              }
          }while((choix<1)||(choix>4));
     }while(choix!=4);
     return 0;
