1b650d0c25a0f71a0fde8281e78593908cecdba2
HabibAroua <habibha.aroua82@gmail.com>
2018-03-30 12:43:56 +0100

    Menu

--- Piles & File/File/main.c
+++ Piles & File/File/main.c
@@ -3,6 +3,20 @@
 
 int main()
 {
-    printf("Hello world!\n");
+    int choix;
+    do
+    {
+         do
+         {
+              printf("__________Menu__________\n");
+              printf("1)Enfiler_______________\n");
+              printf("2)Defiler_______________\n");
+              printf("3)Affichier la file_____\n)");
+              printf("4)Quitter_______________\n");
+              printf("________________________\n");
+              printf("Donner votre choix \n");
+              scanf("%d",&choix);
+         }while((choix<1)||(choix>4))
+    }while(choix!=4);
     return 0;
 }
