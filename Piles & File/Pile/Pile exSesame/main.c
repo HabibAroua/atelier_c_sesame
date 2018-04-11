56f4cba9dbd5e3b0024640c6d6f627674d17da02
HabibAroua <habibha.aroua82@gmail.com>
2018-04-11 22:56:29 +0100

    Declare a pile float

--- Piles & File/Pile/Pile exSesame/main.c
+++ Piles & File/Pile/Pile exSesame/main.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
-#include <stdlib.h>
 
+typedef float TypeDonnee;
+typedef struct
+{
+    int nb_elem; /* nombre d’éléments dans la pile */
+    int nb_elem_max; /* capacité de la pile */
+    TypeDonnee *tab; /* tableau contenant les éléments */
+}Pile;
 int main()
 {
     //waiting the course
