22572d87a4f8d2f85a4b5078deec0992991d2bbc
HabibAroua <habibha.aroua82@gmail.com>
2018-04-19 23:32:39 +0100

    ex 1 tp

--- Piles & File/tp/Ex1/main.c
+++ Piles & File/tp/Ex1/main.c
@@ -1,7 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#define ENTREE "texte.txt"
 
 int main()
 {
+    FILE* fichier = NULL;
+    fichier = fopen(ENTREE, "r+");
+    if (fichier != NULL)
+    {
+        printf("On peut lire et écrire dans le fichier \n");
+    }
+    else
+    {
+        // On affiche un message d'erreur si on veut
+        printf("Impossible d'ouvrir le fichier test.txt");
+    }
     return 0;
 }
