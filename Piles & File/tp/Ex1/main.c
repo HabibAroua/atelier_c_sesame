00927d2b979c24bcde38b7223e173bba27833601
HabibAroua <habibha.aroua82@gmail.com>
2018-04-20 23:30:34 +0100

    try

--- Piles & File/tp/Ex1/main.c
+++ Piles & File/tp/Ex1/main.c
@@ -5,8 +5,21 @@
 #include <errno.h>
 #define ENTREE "texte.txt"
 
+typedef struct Cell
+{
+    char donnee;
+    struct Cell *suivant; /* pointeur sur la cellule suivante */
+}TypeCellule;
+
+typedef TypeCellule* Pile; /* la pile est un pointeur */
+/* sur la tête de liste */
+Pile Initialiser();
+int EstVide(Pile P);
+
 int main()
 {
+    Pile p;
+    p=NULL;
     FILE* fichier = NULL;
     char caractereActuel;
     fichier = fopen(ENTREE, "r+");
@@ -16,6 +29,8 @@ int main()
         {
             caractereActuel = fgetc(fichier); // On lit le caractère
             printf("%c", caractereActuel); // On l'affiche
+            Empiler(&p,caractereActuel);
+            afficher(&p);
         } while (caractereActuel != EOF); // On continue tant que fgetc n'a pas retourné EOF
         fclose(fichier);
     }
@@ -24,5 +39,39 @@ int main()
         // On affiche un message d'erreur si on veut
         printf("Impossible d'ouvrir le fichier test.txt");
     }
+    afficher(&p);
+
     return 0;
+}
+
+Pile Initialiser()
+{
+     return NULL; /* on retourne une liste vide */
+}
+
+void Empiler(Pile* pP, char elem)
+{
+    Pile q;
+    q = (TypeCellule*)malloc(sizeof(TypeCellule)); /* allocation */
+    q->donnee = elem; /* ajout de l’élément à empiler */
+    q->suivant = *pP; /* insertion en tête de liste */
+    *pP =q; /* mise à jour de la tête de liste */
+
+}
+
+
+int EstVide(Pile P)
+{
+     /* renvoie 1 si la liste est vide */
+     return (P == NULL) ? 1 : 0;
+}
+
+void afficher(Pile *p)
+{
+     Pile p1=*p;
+     while(EstVide(p1))
+     {
+          printf("%c \n",p1->donnee);
+          p1=p1->suivant;
+     }
 }
