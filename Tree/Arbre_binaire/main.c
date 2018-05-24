4000a455f1a6b4364a9b7e02eedead1d16d8de54
HabibAroua <habibha.aroua82@gmail.com>
2018-05-24 18:04:34 +0100

    declaration

--- Tree/Arbre_binaire/main.c
+++ Tree/Arbre_binaire/main.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef enum
+{
+    false,
+    true
+} Bool;
+
+typedef struct StackElement
+{
+    int value;
+    struct StackElement *next;
+}StackElement , *Stack;
+
 int main()
 {
     return 0;
